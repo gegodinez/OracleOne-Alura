219d3fe70e2fdb974ab50f6a0cf0685fe74217ba
gegodinez <godinez.gerson@gmail.com>
2023-01-21 16:07:17 -0600

    Arreglo de funciones encriptarTexto y mostrarTexto

--- Challege-01-Encriptador/index.js
+++ Challege-01-Encriptador/index.js
@@ -15,41 +15,52 @@ Extras:
 - Un botón que copie el texto encriptado/desencriptado para la sección de transferencia, o sea que tenga la misma funcionalidad del ctrl+C o de la opción "copiar" del menú de las aplicaciones.
 */
 
+// variables de entrada
+var textIn = [["H", "o", "l", "a"], ["M", "u", "n", "d", "o", "!"], ["M", "i"], ["N", "o", "m", "b", "r", "e"], ["e", "s"], ["G", "e", "r", "s", "o", "n"], ["G", "o", "d", "i", "n", "e", "z", "."]] // "Hola Mundo! Mi nombre es Gerson Godinez."
+
+// varibales de salida
+let textOut = ""
 
 // funncion de encriptacion
-var textIn = [["H", "o", "l", "a"], ["M", "u", "n", "d", "o", "!"], ["M", "i"], ["N", "o", "m", "b", "r", "e"], ["e", "s"], ["G", "e", "r", "s", "o", "n"], ["G", "o", "d", "i", "n", "e", "z", "."]] // "Hola Mundo! Mi nombre es Gerson Godinez."
-for (let i = 0; i < textIn.length; i++) {
-    let word = textIn[i]
-    for (let j = 0; j < word.length; j++) {
-        let letter = word[j]
-        switch (letter) {
-            case "e":
-                word[j] = "enter"
-                break
-            case "i":
-                word[j] = "imes"
-                break
-            case "a":
-                word[j] = "ai"
-                break
-            case "o":
-                word[j] = "ober"
-                break
-            case "u":
-                word[j] = "ufat"
-                break
-            default:
-                break
+function encriptarTexto(textIn, textOut) {
+    for (let i = 0; i < textIn.length; i++) {
+        let word = textIn[i]
+        for (let j = 0; j < word.length; j++) {
+            let letter = word[j]
+            switch (letter) {
+                case "e":
+                    word[j] = "enter"
+                    break
+                case "i":
+                    word[j] = "imes"
+                    break
+                case "a":
+                    word[j] = "ai"
+                    break
+                case "o":
+                    word[j] = "ober"
+                    break
+                case "u":
+                    word[j] = "ufat"
+                    break
+                default:
+                    break
+            }
         }
     }
 }
 
 // funcion de mostrar texto
-let textOut = ""
-for (let i = 0; i < textIn.length; i++){
-    for (let j = 0; j < textIn[i].length; j++) {
-        textOut = textOut + textIn[i][j] 
+function mostrarTexto(textIn, textOut) {
+    for (let i = 0; i < textIn.length; i++){
+        for (let j = 0; j < textIn[i].length; j++) {
+            textOut = textOut + textIn[i][j] 
+        }
+        textOut = textOut + " "
     }
-    textOut = textOut + " "
+    console.log(textOut)
 }
-console.log(textOut)
+
+// Main body
+encriptarTexto(textIn, textOut)
+mostrarTexto(textIn, textOut)
